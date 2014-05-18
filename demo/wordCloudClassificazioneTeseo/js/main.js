945b2904b07ca9e163adfadc4a7485f435e945fb
mfortini <matteo.fortini@gmail.com>
2014-05-18 15:49:44 +0200

    Update main.js

--- demo/wordCloudClassificazioneTeseo/js/main.js
+++ demo/wordCloudClassificazioneTeseo/js/main.js
@@ -8,7 +8,7 @@ d3.json("json/data.json", function(error, root) {
         return {text: d.word, size: d.counter*0.3};
       }))
       .padding(5)
-      .rotate(function() { return ~~(Math.random() * 3) * 40; })
+      .rotate(function() { return ~~(Math.random() * 4) * 40; })
       .font("Impact")
       .fontSize(function(d) { return d.size; })
       .on("end", draw)
