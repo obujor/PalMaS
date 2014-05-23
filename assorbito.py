82757e6acc2400c7175e02d836d3124a9ac4d2ff
Matteo Fortini <matteo.fortini@gmail.com>
2014-05-23 19:13:29 +0200

    Fix scelta ramo corretto per l'assorbimento. Eliminazione nodi con degree = 0

--- assorbito.py
+++ assorbito.py
@@ -53,6 +53,7 @@ while Found:
     else:
         Found = False
 
+#from IPython import embed; embed()
 
 CHUNK=1000
 idddlList = sorted(idddlList)
@@ -68,8 +69,8 @@ while idddlList:
     {
     ?iterDdl a osr:IterDdl; osr:idDdl ?idddl; osr:fase ?fase.
     ?fase osr:progrIter ?progrFase; osr:ddl ?ddl.
-    ?ddl osr:fase ?codiceFase; osr:dataStatoDdl ?dataStato; osr:dataPresentazione ?dataPresentazione;
-    osr:legislatura %s.
+    ?ddl osr:fase ?codiceFase; osr:dataStatoDdl ?dataStato; osr:dataPresentazione ?dataPresentazione; osr:legislatura ?legislatura.
+    FILTER (?legislatura = %s)
     FILTER (?idddl >= %s AND ?idddl <= %s)
     }
     ORDER BY ?ddl ?progrFase
@@ -89,17 +90,23 @@ while idddlList:
                          dataStato=result['dataStato']['value'],
                          dataPresentazione=result['dataPresentazione']['value']
                          )
+#from IPython import embed; embed()
 
-        for iterDdl in set(g.vs['iterDdl']):
-            fasiIter = g.vs(iterDdl=iterDdl)
-            for i in range(len(fasiIter) - 1):
-                fasePrec = fasiIter[i]
-                faseSucc = fasiIter[i+1]
-                g.add_edge(fasePrec, faseSucc, iterDdl=iterDdl)
+for iterDdl in set(g.vs['iterDdl']):
+    fasiIter = g.vs(iterDdl=iterDdl)
+    if len(fasiIter) > 1:
+        print [v['name'] for v in fasiIter]
+    fasiIter = sorted(fasiIter, key=lambda x:x['progrFase'])
+    for i in range(len(fasiIter) - 1):
+        fasePrec = fasiIter[i]
+        faseSucc = fasiIter[i+1]
+        print "%s->%s" % (fasePrec['name'],faseSucc['name'])
+        g.add_edge(fasePrec, faseSucc, iterDdl=iterDdl)
 
+#from IPython import embed; embed()
 
 q_assorbimento = """PREFIX osr: <http://dati.senato.it/osr/>
-SELECT DISTINCT  ?codiceFaseAssorbito  ?codiceFaseAssorbente 
+SELECT DISTINCT  ?codiceFaseAssorbito  ?codiceFaseAssorbente ?progrIterAssorbente
 {
 # tutte le relazioni di assorbimento
 ?iterDdl osr:assorbimento ?ddlAssorbito.
@@ -116,14 +123,14 @@ osr:legislatura ?legislatura.
 ?ddlAssorbente osr:dataStatoDdl ?dataAssorbimento; osr:fase ?codiceFaseAssorbente; osr:progressivoIter ?progrIterAssorbente.
 
 # Potrebbero esserci più di un ddlAssorbente in base a quella data. Prendiamo quello con ProgressivoIter più basso
-{
-    SELECT ?iterDdl (MIN(?progrIterAssorbente) as ?progrIterAssorbente)
-    {
-            ?iterDdl osr:idDdl ?idddl; osr:fase ?faseIterAssorbente.
-            ?faseIterAssorbente osr:ddl ?ddlAssorbente.
-            ?ddlAssorbente osr:dataStatoDdl ?dataAssorbimento; osr:fase ?codiceFaseAssorbente; osr:progressivoIter ?progrIterAssorbente.
-    }
-}
+#{
+#    SELECT ?iterDdl (MIN(?progrIterAssorbente) as ?progrIterAssorbente)
+#    {
+#            ?iterDdl osr:idDdl ?idddl; osr:fase ?faseIterAssorbente.
+#            ?faseIterAssorbente osr:ddl ?ddlAssorbente.
+#            ?ddlAssorbente osr:dataStatoDdl ?dataAssorbimento; osr:fase ?codiceFaseAssorbente; osr:progressivoIter ?progrIterAssorbente.
+#    }
+#}
 
 FILTER  (?legislatura = %s).
 }
@@ -136,14 +143,28 @@ results = sparql.query().convert()
 for result in results["results"]["bindings"]:
     codiceFaseAssorbito  = result['codiceFaseAssorbito']['value']
     codiceFaseAssorbente = result['codiceFaseAssorbente']['value']
+    progrIterAssorbente  = result['progrIterAssorbente']['value']
     print "%s->%s" % (codiceFaseAssorbito, codiceFaseAssorbente)
     vertexFrom = g.vs.find(name=codiceFaseAssorbito)
     vertexTo   = g.vs.find(name=codiceFaseAssorbente)
-    g.add_edge(vertexFrom,vertexTo,tipo='assorbimento')
+    g.add_edge(vertexFrom,vertexTo,tipo='assorbimento', progrIterAssorbente=progrIterAssorbente)
+
+for v in g.vs(_outdegree_gt=1):
+    ev = g.es(_source=v.index)
+    ev = sorted(ev,key=lambda x:x['progrIterAssorbente'])
+    g.delete_edges(ev[1:])
+
+g.delete_vertices(g.vs(_degree=0))
+
+#from IPython import embed; embed()
 
 
 nodes=[{'name':v['name']} for v in g.vs]
 links=[{'source':e.source,'target':e.target,'value':1.0} for e in g.es]
+
+g.write('assorbito.pickle')
+
+from IPython import embed; embed()
 
 import json
 
