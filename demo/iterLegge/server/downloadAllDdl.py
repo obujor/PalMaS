3a0fc0c7b258a3f5f7aad4f7ba27f08440c31669
Octavian Bujor <octavian.bujor@unibo.it>
2014-05-23 13:57:15 +0200

    Update server scripts in iterLegge demo

--- demo/iterLegge/server/downloadAllDdl.py
+++ demo/iterLegge/server/downloadAllDdl.py
@@ -4,6 +4,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from datetime import datetime
 import sys
 import json
+import time
 
 LEGISLATURA=17
 
@@ -34,7 +35,7 @@ def getAssegnazioniByFaseNr(legislatura, faseNr):
         ?assegnazione osr:dataAssegnazione ?dataAssegnazione.
         ?assegnazione rdfs:label ?label.
         ?fase osr:fase "%s"^^<http://www.w3.org/2001/XMLSchema#string>.
-        ?ddl osr:legislatura %s.
+        ?ddl osr:legislatura %d.
     }""" % (faseNr, legislatura)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
@@ -49,42 +50,62 @@ def getAssegnazioniByFaseNr(legislatura, faseNr):
         groups[date].append(row)
     
     return groups
-    
+ 
+def getDataByIdDdl(legislatura, idddl):
+	idddl = int(idddl)
+	query = """PREFIX osr: <http://dati.senato.it/osr/>
+	SELECT DISTINCT ?stato ?dataPresentazione ?titolo ?fase ?idfase ?progressivoIter ?idddl ?ramo ?natura ?numeroFase ?dataStato
+	{
+		?ddl a osr:Ddl; osr:legislatura %d; osr:statoDdl ?stato; osr:dataStatoDdl ?dataStato; osr:dataPresentazione ?dataPresentazione;
+		osr:titolo ?titolo; osr:fase ?fase; osr:idFase ?idfase; osr:progressivoIter ?progressivoIter; osr:idDdl ?idddl; osr:idDdl %d;
+		osr:ramo ?ramo; osr:natura ?natura; osr:numeroFase ?numeroFase.
+	}""" % (legislatura, idddl,)
+
+	sparql.setQuery(query)
+	sparql.setReturnFormat(JSON)
+	results = sparql.query().convert()
+
+	rows = cleanData(results)
+
+	for row in rows:
+		row["assegnazioni"] = getAssegnazioniByFaseNr(legislatura, row["fase"])
+		
+	return rows
+	
+def downloadAllDdl(legislatura):
+	counter = 0
+	query = """PREFIX osr: <http://dati.senato.it/osr/>
+	SELECT DISTINCT ?idddl
+	{
+		?ddl a osr:Ddl; osr:legislatura %d; osr:idDdl ?idddl.
+	}""" % (legislatura,)
+
+	sparql.setQuery(query)
+	sparql.setReturnFormat(JSON)
+	results = sparql.query().convert()
+
+	rows = cleanData(results)
+	results = []
+	for row in rows:
+		counter+=1
+		print(row['idddl'], " ", counter)
+		results.append(getDataByIdDdl(legislatura, row['idddl']))
+		
+	return results
     
 if len(sys.argv) > 2:
-    FASE=sys.argv[1]
+    LEGISLATURA=int(sys.argv[1])
     OUTPUTFILE=sys.argv[2]
 else:
-    print("Usage: downdloadDdl.py nrFase outputFile")
+    print("Usage: downdloadAllDdl.py legislatura outputFile")
     exit()
 
-print("ELABORO FASE %s" % (FASE,))
+print("ELABORO LEG. %s" % (LEGISLATURA,))
 
 sparql = SPARQLWrapper("http://dati.senato.it/sparql")
+#rows = getDataByIdDdl(LEGISLATURA, 38775)
 
-# ddl e legislatura
-q_ddl_legislatura = """PREFIX osr: <http://dati.senato.it/osr/>
-SELECT DISTINCT ?stato ?dataPresentazione ?titolo ?fase ?idfase ?progressivoIter ?idddl ?ramo ?natura ?numeroFase ?dataStato
-{
-    ?ddl a osr:Ddl; osr:legislatura %d; osr:statoDdl ?stato; osr:dataStatoDdl ?dataStato; osr:dataPresentazione ?dataPresentazione;
-    osr:titolo ?titolo; osr:fase ?fase; osr:idFase ?idfase; osr:progressivoIter ?progressivoIter; osr:idDdl ?idddl;
-    osr:ramo ?ramo; osr:natura ?natura; osr:numeroFase ?numeroFase.
-    {
-     SELECT DISTINCT ?idddl
-    {
-     ?ddl osr:legislatura %d; osr:fase "%s"^^<http://www.w3.org/2001/XMLSchema#string>; osr:idDdl ?idddl.
-    }
-  }
-}""" % (LEGISLATURA, LEGISLATURA, FASE,)
-
-sparql.setQuery(q_ddl_legislatura)
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-
-rows = cleanData(results)
-
-for row in rows:
-    row["assegnazioni"] = getAssegnazioniByFaseNr(LEGISLATURA, row["fase"])
+rows = downloadAllDdl(LEGISLATURA)
 
 with open(OUTPUTFILE, 'w') as outfile:
      json.dump(rows, outfile, indent = 4, ensure_ascii=False)
